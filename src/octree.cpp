e296c8955eacbd4131598c1f9ebd268ad08f4287
Linus Schaub <linusschaub@googlemail.com>
2022-12-12 17:05:39 +0100

    Removed unused code

--- src/octree.cpp
+++ src/octree.cpp
@@ -2,13 +2,9 @@
 
 #include <iostream>
 
-Octree::Octree() : aabb(), points()
-{
-}
+Octree::Octree() : aabb(), points() {}
 
-Octree::Octree(std::vector<Point *> points) : Octree(points, 4)
-{
-}
+Octree::Octree(std::vector<Point *> points) : Octree(points, 4) {}
 
 Octree::Octree(std::vector<Point *> points, int max_depth) : aabb(points), points(), max_depth(max_depth)
 {
@@ -23,15 +19,13 @@ Octree::Octree(Point lower, Point upper) : Octree(lower, upper, 4) {}
 Octree::Octree(Point lower, Point upper, int max_depth) : aabb(lower, upper), points(), max_depth(max_depth) {}
 
 Octree::Octree(AABB aabb) : Octree(aabb, 4) {}
+
 Octree::Octree(AABB aabb, int max_depth) : aabb(aabb), max_depth(max_depth), points(0) {}
 
 bool Octree::insert(Point *p)
 {
-    // TODO: Check if point is already in octree
-
     if (aabb.includes(p) == false)
     {
-        // std::cout << "Not included! " << std::endl;
         return false;
     }
     if (children.size() == 0)
@@ -47,18 +41,13 @@ bool Octree::insert(Point *p)
         // }
         if (points.size() == 0 || max_depth < 1)
         {
-            if (max_depth < 1)
-            {
-                // std::cout << "Reached max depth in " << aabb << ". " << std::endl;
-            }
-            // std::cout << "Initial point in " << aabb << ". " << std::endl;
+            // Reached max depth
             points.push_back(p);
             return true;
         }
         else
         {
             // Subdivide
-            // std::cout << "Subdividing octree. " << std::endl;
             std::vector<AABB *> aabbs = aabb.subdivide();
             for (int i = 0; i < points.size(); i++)
             {
